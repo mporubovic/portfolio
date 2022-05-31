70e64ac2a5a817073c547ac5150db672422f2326
mporubovic <mawej1@gmail.com>
2022-05-31 20:22:21 +0100

    set card to return to original position in stack

--- src/Stack/Stack.js
+++ src/Stack/Stack.js
@@ -13,9 +13,9 @@ backgroundBlendMode: "soft-light, soft-light, overlay, screen, normal"}},
     // {text: "Portfolio", background: {background: "radial-gradient(100% 225% at 100% 0%, #120037 0%, #000000 100%), linear-gradient(35deg, #C0FFC7 0%, #17001F 75%), linear-gradient(55deg, #2400FF 0%, #000000 100%), linear-gradient(90deg, #FFE037 0%, #FFE037 40%, #1DCD9F 40%, #1DCD9F 50%, #088C6F 50%, #088C6F 70%, #23033C 70%, #23033C 100%), linear-gradient(180deg, #FF8FE5 0%, #FF8FE5 45%, #FBFF64 45%, #FBFF64 60%, #76E3FF 60%, #76E3FF 80%, #6EB6E7 80%, #6EB6E7 100%)",
 // backgroundBlendMode: "screen, overlay, overlay, darken, normal"}},
     {text: "Cube", component: Cube, background: {background: "conic-gradient(from 30deg, rgb(53 53 53) 0%, rgb(15 15 15) 33.3%, rgb(38 38 38) 33.4%, rgb(28 28 28) 66.6%, rgb(54 54 54) 66.7%, rgb(33 33 33) 100%, rgb(0 0 0) 100%)"}},
-    {text: "Society Website", background: {background: "linear-gradient(180deg, #A90051 0%, #1E0031 100%), linear-gradient(121.28deg, #340067 0%, #000000 38.54%), linear-gradient(180deg, #00FF19 0%, #000000 100%), linear-gradient(294.84deg, #FF0000 0%, #450000 100%), linear-gradient(121.28deg, #32003A 0%, #FF4040 100%), radial-gradient(50% 72.12% at 50% 50%, #EB00FF 0%, #110055 100%)",
+    {text: "dSoc Website", background: {background: "linear-gradient(180deg, #A90051 0%, #1E0031 100%), linear-gradient(121.28deg, #340067 0%, #000000 38.54%), linear-gradient(180deg, #00FF19 0%, #000000 100%), linear-gradient(294.84deg, #FF0000 0%, #450000 100%), linear-gradient(121.28deg, #32003A 0%, #FF4040 100%), radial-gradient(50% 72.12% at 50% 50%, #EB00FF 0%, #110055 100%)",
 backgroundBlendMode: "color-dodge, screen, overlay, exclusion, difference, normal"}},
-    {text: "Society Designs", background: {background: "linear-gradient(235deg, #FFFFFF 0%, #000F25 100%), linear-gradient(180deg, #6100FF 0%, #000000 100%), linear-gradient(235deg, #FFA3AC 0%, #FFA3AC 40%, #00043C calc(40% + 1px), #00043C 60%, #005D6C calc(60% + 1px), #005D6C 70%, #00C9B1 calc(70% + 1px), #00C9B1 100%), linear-gradient(125deg, #FFA3AC 0%, #FFA3AC 40%, #00043C calc(40% + 1px), #00043C 60%, #005D6C calc(60% + 1px), #005D6C 70%, #00C9B1 calc(70% + 1px), #00C9B1 100%)",
+    {text: "dSoc Designs", background: {background: "linear-gradient(235deg, #FFFFFF 0%, #000F25 100%), linear-gradient(180deg, #6100FF 0%, #000000 100%), linear-gradient(235deg, #FFA3AC 0%, #FFA3AC 40%, #00043C calc(40% + 1px), #00043C 60%, #005D6C calc(60% + 1px), #005D6C 70%, #00C9B1 calc(70% + 1px), #00C9B1 100%), linear-gradient(125deg, #FFA3AC 0%, #FFA3AC 40%, #00043C calc(40% + 1px), #00043C 60%, #005D6C calc(60% + 1px), #005D6C 70%, #00C9B1 calc(70% + 1px), #00C9B1 100%)",
 backgroundBlendMode: "soft-light, screen, darken, normal"}},
     {text: "Tutoring App", background: {background: "radial-gradient(65% 100% at 50% 0%, #00FF94 0%, rgba(0, 255, 148, 0.25) 100%), linear-gradient(230deg, #000000 25%, #170059 100%), linear-gradient(215deg, #FFEBB9 10%, #19004E 80%), radial-gradient(100% 245% at 100% 100%, #FFFFFF 0%, #000353 100%), linear-gradient(125deg, #1400FF 0%, #3A0000 100%), linear-gradient(225deg, #00F0FF 30%, #000B6F 45%, #00EBFC 45%, #001676 65%, #00E1F6 65%, #001676 85%, #00ECFD 85%, #001676 100%), linear-gradient(135deg, #00F0FF 0%, #000B6F 15%, #00EBFC 15%, #001676 35%, #00E1F6 35%, #001676 55%, #00ECFD 55%, #001676 100%)",
 backgroundBlendMode: "soft-light, screen, overlay, overlay, difference, overlay, normal"}},
@@ -79,7 +79,7 @@ export default function Stack() {
         // debugger
 
         let insertionIndex = 0
-        // while (cardsInStack[insertionIndex] < cardId) insertionIndex ++
+        while (cardsInStack[insertionIndex] > cardId) insertionIndex ++
 
         cardsInStack.splice(insertionIndex, 0, cardId)
 
@@ -96,7 +96,7 @@ export default function Stack() {
 
         // let insertionIndex = cardsInStack.length
         let insertionIndex = 0
-        // while (cardOnBoard > cardsInStack[insertionIndex]) insertionIndex ++
+        while (cardsInStack[insertionIndex] > cardOnBoard) insertionIndex ++
 
         cardsInStack.splice(insertionIndex, 0, cardOnBoard)
 
