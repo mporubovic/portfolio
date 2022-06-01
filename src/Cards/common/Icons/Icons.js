ce65e7d7c1bf89d6bb8a389c9774d72bc8f94a80
mporubovic <mawej1@gmail.com>
2022-06-02 00:08:18 +0100

    fix java icon size

--- src/Cards/common/Icons/Icons.js
+++ src/Cards/common/Icons/Icons.js
@@ -1,13 +1,14 @@
 import styles from './Icons.module.css'
 
 export default function Icons(props) {
+    const mobile = window.innerWidth <= 500
 
     return (
         <div className="content-card" style={props.style}>
             {
                 props.icons.map((icon, i) => (
-                    <img className={styles.icon} key={i} src={icon} style={{
-                        height: props.height
+                    <img key={i} src={icon} style={{
+                        height: mobile ? (icon.includes("java") ? "40px" : "20px") : (icon.includes("java") ? "50px" : props.height)// horrible I know
                     }}  />
                 ))
             }
