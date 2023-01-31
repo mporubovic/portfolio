27edeb3e9a7b93b20b1dfedb280f75cd0a3405bf
mporubovic <mawej1@gmail.com>
2023-01-31 02:49:58 +0000

    update copy, buttons, card style

--- src/Cards/AboutMe/AboutMe.js
+++ src/Cards/AboutMe/AboutMe.js
@@ -5,6 +5,7 @@ import Icons from "../common/Icons/Icons";
 import icons from "../common/Icons/iconAssets";
 
 const name = "Matej Porubovic"
+const nameZH = "马杰"
 
 const phone = "+44 7539 466503"
 const phoneHref = "tel:+447539466503"
@@ -16,17 +17,19 @@ const linkedin = "https://www.linkedin.com/in/matej-porubovic/"
 const github = "https://github.com/mporubovic"
 const discord = "https://discord.com/users/213766571942871041"
 
-const jsZoomPanHref = "https://github.com/mporubovic/zoomable.js"
+const canvasAppHref = "https://canvas.porubovic.sk/"
 const linksHref= "https://links.porubovic.sk/"
+const jsZoomPanHref = "https://github.com/mporubovic/zoomable.js"
 
-const cv = "/cv.pdf"
+const cvHref = "/cv.pdf"
 
 const text = [
-    "Welcome to my portfolio! Click on any project to learn more.",
-    "I’ve done 2 years of Engineering and a year of Computer Science, learning the basics of algorithms, data management and OOP. My programming knowledge is listed below.",
+    "Welcome to my portfolio! I am a London-based software engineer. This is a showcase of projects I've done over the years - click on the cards to learn more.",
+    "I’ve done 2 years of Engineering and a year of Computer Science, learning the basics of algorithms, data management and OOP. " +
+    `You can have a look at my CV <a href=${cvHref} target='_blank'><u>here.</u></a>`,,
     "I really enjoy web development, currently improving my React skills and learning TypeScript. I also design UIs, and I use Figma daily.",
-    "In my spare time, if I’m not programming or designing, I learn Chinese (普通话) and read books; my favourites are Sapiens and I, Robot.",
-    "Pro tip: Command/Ctrl + drag to re-arrange cards in the stack."
+    "In my spare time, if I’m not programming or designing, I learn Chinese (Mandarin) and read books; my favourites are Sapiens and I, Robot.",
+    "<i>Pro tip: Command/Ctrl + drag to re-arrange cards in the stack.</i>"
 ]
 
 export default function AboutMe(props) {
@@ -38,14 +41,15 @@ export default function AboutMe(props) {
                 <div className={styles.sectionA}>
                     <div className="content-card">
                         <img src={pfp} style={{
-                            height: "80px",
+                            height: "100px",
                             borderRadius: "999px"
                         }} />
-                        <h1 style={{
+                        <div style={{
                             marginLeft: "20px"
                         }}>
-                            {name}
-                        </h1>
+                            <h1>{name}</h1>
+                            <h2>{nameZH}</h2>
+                        </div>
                     </div>
 
                     <div className="content-card" style={{
@@ -55,7 +59,7 @@ export default function AboutMe(props) {
                         alignItems: "flex-start",
                     }}>
                         { text.map((t, i) => (
-                            <span key={i}>{t}</span>
+                            <span key={i} dangerouslySetInnerHTML={{__html: t}}></span>
                         )) }
                     </div>
 
@@ -119,7 +123,7 @@ export default function AboutMe(props) {
                                       style={{ flex: "auto", gridArea: "2 / 1 / 3 / 2" }}
                         />
 
-                        <ActionButton text="CV" href={cv}
+                        <ActionButton text="CV" href={cvHref}
                                       style={{ gridArea: "2 / 2 / 3 / 3" }}
                         />
 
@@ -141,14 +145,18 @@ export default function AboutMe(props) {
                             gridTemplate: "1fr 1fr / 1fr 1fr",
                             gap: "15px"
                         }}>
-
-
-
                             <ActionButton icon={icons.cards.cube}
                                           iconSize="50px"
                                           text="Cube"
                                           flexDirection="column"
                                           callback={() => props.stackContext.drawCard(1)}
+                            />
+
+                            <ActionButton icon={icons.touchpad}
+                                          iconSize="50px"
+                                          text="Canvas app"
+                                          flexDirection="column"
+                                          href={canvasAppHref}
                             />
 
                             <ActionButton icon={icons.cards.mylayr}
@@ -158,21 +166,12 @@ export default function AboutMe(props) {
                                           callback={() => props.stackContext.drawCard(4)}
                             />
 
-                            <ActionButton icon={icons.cards.dsoc}
-                                          iconSize="50px"
-                                          text="dSoc website"
-                                          flexDirection="column"
-                                          callback={() => props.stackContext.drawCard(2)}
-                            />
-
                             <ActionButton icon={icons.figma_white}
                                           iconSize="50px"
                                           text="dSoc designs"
                                           flexDirection="column"
                                           callback={() => props.stackContext.drawCard(3)}
                             />
-
-
                         </div>
                     </div>
 
@@ -181,10 +180,9 @@ export default function AboutMe(props) {
                         gridTemplate: "1fr 1fr / 1fr 1fr",
                         gap: "15px"
                     }}>
-
-                        <ActionButton icon={icons.touchpad}
-                                      text="JS zoom/pan"
-                                      href={jsZoomPanHref}
+                        <ActionButton icon={icons.cards.dsoc}
+                                      text="dSoc website"
+                                      callback={() => props.stackContext.drawCard(2)}
                         />
 
                         <ActionButton icon={icons.camera}
@@ -197,9 +195,9 @@ export default function AboutMe(props) {
                                       href={linksHref}
                         />
 
-                        <ActionButton icon={icons.document}
-                                      text="WIP"
-                                      callback={() => null}
+                        <ActionButton icon={icons.touchpad}
+                                      text="JS zoom/pan"
+                                      href={jsZoomPanHref}
                         />
                     </div>
                 </div>
